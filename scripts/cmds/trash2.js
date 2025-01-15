8758839c89df2e7b429d5c7f8326412162128166
Itz HaSan <146573168+hasan32425@users.noreply.github.com>
2025-01-15 23:05:02 +0600

    Update trash2.js

--- scripts/cmds/trash2.js
+++ scripts/cmds/trash2.js
@@ -1,4 +1,4 @@
-/cmd install trash2.js const fs = require("fs");
+const fs = require("fs");
 const path = require("path");
 // Ensure DIG is properly imported
 const DIG = require("discord-image-generation"); 
