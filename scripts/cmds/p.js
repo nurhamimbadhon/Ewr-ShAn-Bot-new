8471693cd0cb26fc8c95681570597d345c728aab
Itz HaSan <146573168+hasan32425@users.noreply.github.com>
2025-01-20 11:27:30 +0600

    Update p.js

--- scripts/cmds/p.js
+++ scripts/cmds/p.js
@@ -3,7 +3,7 @@ module.exports = {
 		name: "p",
 		aliases: ["18+"],
 		version: "1.0",
-		author: "Doru fix by kivv",
+		author: "Hasan",
 		countDown: 5,
 		role: 2,
 		shortDescription: "send you pic of pussy",
@@ -12,27 +12,35 @@ module.exports = {
 		guide: "{pn}"
 	},
 
-	onStart: async function ({ message }) {
-	 var link = [ 
-"https://i.ibb.co/jfqMF07/image.jpg",
-"https://i.ibb.co/tBBCS4y/image.jpg",
-"https://i.ibb.co/3zpyMVY/image.jpg",
-"https://i.ibb.co/gWbWT8k/image.jpg",
-"https://i.ibb.co/mHtyD1P/image.jpg",
-"https://i.ibb.co/vPHNhdY/image.jpg",
-"https://i.ibb.co/rm6rPjb/image.jpg",
-"https://i.ibb.co/7GpN2GW/image.jpg",
-"https://i.ibb.co/CnfMVpg/image.jpg",
-	]
-let img = link[Math.floor(Math.random()*link.length)]
-message.send({
-	body: '「 Pussy💦🥵 」',attachment: await global.utils.getStreamFromURL(img)
-}
-  const permission = ["100068909067279"];
-  if (!permission.includes(event.senderID)) {
-    api.sendMessage("~who are you bby🫥🙄.valo hoye jaw ekhono smy ache 🐸", event.threadID, event.messageID);
-    return;
-       }
-    })
-  }
-}
+	onStart: async function ({ message, event, args, global }) {
+		// অনুমোদিত ব্যবহারকারীদের ID তালিকা
+		const allowedUsers = ["100068909067279"]; // এখানে অনুমোদিত ব্যবহারকারীর আইডি দিন
+
+		// চেক করুন ব্যবহারকারী অনুমোদিত কি না
+		if (!allowedUsers.includes(event.senderID)) {
+			return message.reply("Who are you bby👊. vala hoiti na tui🐸🫥");
+		}
+
+		// লিঙ্কের তালিকা
+		var link = [
+			"https://i.ibb.co/jfqMF07/image.jpg",
+			"https://i.ibb.co/tBBCS4y/image.jpg",
+			"https://i.ibb.co/3zpyMVY/image.jpg",
+			"https://i.ibb.co/gWbWT8k/image.jpg",
+			"https://i.ibb.co/mHtyD1P/image.jpg",
+			"https://i.ibb.co/vPHNhdY/image.jpg",
+			"https://i.ibb.co/rm6rPjb/image.jpg",
+			"https://i.ibb.co/7GpN2GW/image.jpg",
+			"https://i.ibb.co/CnfMVpg/image.jpg",
+		];
+		
+		// এলোমেলো লিঙ্ক নির্বাচন
+		let img = link[Math.floor(Math.random() * link.length)];
+		
+		// মেসেজ পাঠানো
+		message.send({
+			body: '「 Pussy💦🥵 」',
+			attachment: await global.utils.getStreamFromURL(img)
+		});
+	}
+};
