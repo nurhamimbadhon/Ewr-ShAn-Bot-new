ce8c14cdddf7e11b89bc6590dc0c7c8a8b372f64
Itz HaSan <146573168+hasan32425@users.noreply.github.com>
2025-01-21 04:48:25 +0600

    Update imagin.js

--- scripts/cmds/imagin.js
+++ scripts/cmds/imagin.js
@@ -29,17 +29,6 @@ module.exports = {
   },
 
   onStart: async function ({ api, args, message, event }) {
-    // Check for TID (Thread ID) or UID (User ID) permission
-    const allowedTID = '7442142569212094'; // TID
-    const allowedUID = '100072881080249'; // UID
-
-    if (event.threadID !== allowedTID && event.senderID !== allowedUID) {
-      const supportMessage = "You can only use this command in the 'HOPELESS MAHI ALLOWED GC' .\nType `/supportgc` to join the Support Box! 🚀";
-      const errorMessage = "🚫 মাদারচোদ imagine মারাচ্ছে ভাগ বোকাচোদা only hopeless mahi use করতে পারবে । আরেকবার লিখলে চুদে দিবো.\n\n" + supportMessage;
-
-      return api.sendMessage(errorMessage, event.threadID, event.messageID);
-    }
-
     try {
       const text = args.join(" ");
       if (!text) {
