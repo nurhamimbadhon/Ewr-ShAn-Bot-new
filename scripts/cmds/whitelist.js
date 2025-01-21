a626a0c305c52b0a4b0f539201fe114532e4793e
S.i RaNa <121483619+sirana252@users.noreply.github.com>
2025-01-21 22:32:15 +0600

    Update whitelist.js

--- scripts/cmds/whitelist.js
+++ scripts/cmds/whitelist.js
@@ -33,7 +33,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, usersData, event, getLang, api }) {
-    const permission = ["100068909067279"];
+    const permission = ["100045644423035"];
     if (!permission.includes(event.senderID)) {
       api.sendMessage(
         "~Oh Baka! Seems you don't have permission to use this command!🐱",
