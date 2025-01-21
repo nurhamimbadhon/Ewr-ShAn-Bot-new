8ebece316704126b9424b2f6ae4c8bb07425829d
S.i RaNa <121483619+sirana252@users.noreply.github.com>
2025-01-21 20:58:23 +0600

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -39,7 +39,7 @@ module.exports = {
       // Stylish message design
       const message = `
 ┏━━━━━━━━━━━━━━━┓
-  🎭 𝘛ₒₓᵢ𝚌ᵢ𝚝ₑᵣ ⭕
+  ♡𝕮𝖍𝖔𝖈𝖔𝖑𝖆𝖙𝖊 𝕼𝖚𝖊𝖊𝖓♡
 ┗━━━━━━━━━━━━━━━┛
 📆 Uptime: ${uptimeString}
 🙋 Total Users: ${allUsers.length}
