2369c5d6b2bbefd32a1515e5fd18ebde3006a095
S.i RaNa <121483619+sirana252@users.noreply.github.com>
2025-01-21 18:50:53 +0600

    Update hlw.js

--- scripts/cmds/hlw.js
+++ scripts/cmds/hlw.js
@@ -85,7 +85,7 @@ const talkWithBot = async (api, threadID, messageID, senderID, input) => {
 
 module.exports.config = {
   name: "bbu",
-  aliases: ["bbz", "hey"],
+  aliases: ["Shan", "shan", "bbz", "bby"],
   version: "1.6.9",
   author: "Nazrul",
   role: 0,
