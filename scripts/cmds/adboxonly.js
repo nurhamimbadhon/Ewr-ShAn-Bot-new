6e5cc98efee0c1c0920bcaff4bda2d6bd749cead
S.i RaNa <121483619+sirana252@users.noreply.github.com>
2025-02-03 22:28:36 +0600

    Update adboxonly.js

--- scripts/cmds/adboxonly.js
+++ scripts/cmds/adboxonly.js
@@ -10,7 +10,7 @@ module.exports = {
 			vi: "bật/tắt chế độ chỉ quản trị của viên nhóm mới có thể sử dụng bot",
 			en: "turn on/off only admin box can use bot"
 		},
-		category: "box chat",
+		category: "𝗕𝗢𝗫 𝗖𝗛𝗔𝗧",
 		guide: {
 			vi: "   {pn} [on | off]: bật/tắt chế độ chỉ quản trị viên nhóm mới có thể sử dụng bot"
 				+ "\n   {pn} noti [on | off]: bật/tắt thông báo khi người dùng không phải là quản trị viên nhóm sử dụng bot",
